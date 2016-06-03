c30d18d1daaae017215108497b478a9a7a79ead9
Brett New <brettnew@Bretts-MacBook-Air.local>
2016-06-03 15:25:23 -0500

    It will now have more styles from CSS and branching in jQuery; will display results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
   $("#choose").click(function(){
     var vacation = parseInt($("#answer1").val()) + parseInt($("#answer2").val()) + parseInt($("#answer3").val()) + parseInt($("#answer4").val()) + parseInt($("#answer5").val()) + parseInt($("#answer6").val());
-    alert (vacation);
-    debugger
-    if (vacation <=3 ){
-      $("#iceland").show();
+    var beach = $("input:radio[name=beach]:checked").val();
+
+    if (beach === "tropical"){
+      $("#costarica").toggle();
+      $("#iceland #pei #dubai").hide();
+    } else if (vacation <=3){
+      $("#iceland").toggle();
       $("#pei #costarica #dubai").hide();
     } else if (3 < vacation && vacation <= 9){
-      $("#pei").show();
+      $("#pei").toggle();
       $("#iceland #costarica #dubai").hide();
     } else if (9 < vacation && vacation <= 15){
-      $("#costarica").show();
-      $("#iceland #peid #dubai").hide();
+      $("#costarica").toggle();
+      $("#iceland #pei #dubai").hide();
     } else if (15 < vacation && vacation <= 18){
-      $("#dubai").show();
+      $("#dubai").toggle();
       $("#iceland #costarica #pei").hide();
     } else if (vacation >=100){
       alert("Please answer all questions.")
